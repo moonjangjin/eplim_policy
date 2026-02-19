44f103066f2909a813a71750a0c17148a5a1e1e9
moonjangjin <152685252+moonjangjin@users.noreply.github.com>
2026-02-19 15:03:10 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  base: '/eplim_policy/',
-  plugins: [react(), tailwindcss()],
-  define: {
-    'process.env.GEMINI_API_KEY': JSON.stringify(process.env.GEMINI_API_KEY)
-  }
-})
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      base: '/eplim_policy/',
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [react(), tailwindcss()],
+      define: {
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      }
+    };
+});
